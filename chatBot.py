b632981a1160c8ad2490f09a5bf5e404b0f675cd
ireneadler123 <145485199+ireneadler123@users.noreply.github.com>
2024-03-14 12:46:43 +0700

    Update chatBot.py

--- chatBot.py
+++ chatBot.py
@@ -13,7 +13,7 @@ openai = OpenAI(OPENAI_API_KEY)
 
 st.set_page_config(layout='wide', page_title='Học Tiếng Anh cùng trợ lý ảo Mia', page_icon='heart')
 # Set API key for OpenAI. Replace 'YOUR_OPENAI_API_KEY' with your actual API key.
-openai.api_key = secure
+openai.api_key = openai
 
 name = st.text_input('Hãy cho Mia xin tên của bạn nhé')
 
